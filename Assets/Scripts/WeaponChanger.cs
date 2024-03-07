4e3d1bffb89f784e5c8ba24b2500313c389dc134
Alperen <alpsutcu2001@gmail.com>
2024-03-07 22:22:50 +0300

    Update WeaponChange

---
+++ Assets/Scripts/WeaponChanger.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Change_Gun : MonoBehaviour
+{
+    public int SelectedWeapon = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SelectWeapon();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        int prevriousweapon = SelectedWeapon;
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if (SelectedWeapon >= transform.childCount - 1)
+            {
+                SelectedWeapon = 0;
+            }
+            else
+                SelectedWeapon++;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if (SelectedWeapon <= 0)
+            {
+                SelectedWeapon = transform.childCount - 1;
+            }
+            else
+                SelectedWeapon--;
+        }
+        if (prevriousweapon != SelectedWeapon)
+        {
+            SelectWeapon();
+        }
+    }
+    void SelectWeapon()
+    {
+        int i = 0;
+        foreach (Transform weapon in transform)
+        {
+            if (i == SelectedWeapon)
+                weapon.gameObject.SetActive(true);
+            else
+                weapon.gameObject.SetActive(false);
+            i++;
+        }
+    }
+
+}
